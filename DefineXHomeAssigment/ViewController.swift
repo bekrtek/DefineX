e99096ddf68d798db11613ee9ea87e3eb0485e86
Bekir Tek <bekirtek.tk@gmail.com>
2025-01-09 16:08:15 +0300

    Initial Commit

---
+++ DefineXHomeAssigment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DefineXHomeAssigment
+//
+//  Created by Bekir Tek on 9.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
